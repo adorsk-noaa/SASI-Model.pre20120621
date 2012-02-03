bcbd5d5096f26bee635c5cff2aa77a1921c56de5
adorsk <adorsk>
2012-02-03 16:27:12 -0500

    changed va method naming for pep8 convention

--- sasi/va/tests/test_va.py
+++ sasi/va/tests/test_va.py
@@ -8,8 +8,8 @@ class VulnerabilityAssessmentTest(unittest.TestCase):
 	def test(self):
 		va_rows = sasi.util.va.read_va_from_csv(sasi.conf.conf['va_file'])
 		va = VulnerabilityAssessment(rows = va_rows)	
-		f_by_h = va.getFeaturesByHabitats()
-		features = va.getFeatures()
+		f_by_h = va.get_features_by_habitats()
+		features = va.get_features()
 		print features
 if __name__ == '__main__':
 	unittest.main()
--- sasi/va/va.py
+++ sasi/va/va.py
@@ -2,9 +2,9 @@ class VulnerabilityAssessment(object):
 
 	def __init__(self, rows=[]):
 
-		self.assessments = self.makeAssessmentsFromRows(rows)
+		self.assessments = self.make_assessments_from_rows(rows)
 	
-	def makeAssessmentsFromRows(self, rows):
+	def make_assessments_from_rows(self, rows):
 		# Save rows, keyed by (GEAR_CODE, SUBSTRATE_CODE, FEATURE_CODE,ENERGY)
 		assessments = {}
 		for r in rows:
@@ -12,7 +12,7 @@ class VulnerabilityAssessment(object):
 			assessments[key] = r
 		return assessments
 
-	def getFeaturesByHabitats(self):
+	def get_features_by_habitats(self):
 		f_by_h = {}
 		for key, assessment in self.assessments.items():
 			hab_key = (assessment['SUBSTRATE_CODE'], assessment['ENERGY'])
@@ -21,7 +21,7 @@ class VulnerabilityAssessment(object):
 			features_for_category.add(assessment['FEATURE_CODE'])
 		return f_by_h
 
-	def getFeatures(self):
+	def get_features(self):
 		features = {} 
 		for key, assessment in self.assessments.items():
 			features[assessment['FEATURE_CODE']] = {
@@ -30,4 +30,14 @@ class VulnerabilityAssessment(object):
 					'FEATURE': assessment['FEATURE']
 					}
 		return features
+
+	def get_features_by_habitats_energy(self):
+		f_by_h_e = {}
+		for key, assessment in self.assessments.items():
+			hab_key = (assessment['SUBSTRATE_CODE'], assessment['ENERGY'])
+			features_for_hab = f_by_h.setdefault(hab_key,{})
+			features_for_category = features_for_hab.setdefault(assessment['FEATURE_CLASS_CODE'],set())
+			features_for_category.add(assessment['FEATURE_CODE'])
+		return f_by_h
+
 
