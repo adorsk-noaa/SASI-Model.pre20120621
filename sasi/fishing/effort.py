642ae93c27b66959f9552edfc13560b13293d8e1
adorsk <adorsk>
2012-02-07 14:54:45 -0500

    added start of fishing models

---
+++ sasi/fishing/effort.py
@@ -0,0 +1,8 @@
+class Effort(object):
+
+	def __init__(self, id=None, area=None, gear=None, location=None, time=None):
+		self.id = id
+		self.area = area
+		self.gear = gear
+		self.location = location
+		self.time = time
---
+++ sasi/fishing/gear.py
@@ -0,0 +1,5 @@
+class Gear(object):
+
+	def __init__(self, id=None, name=None):
+		self.id = id
+		self.name = name
