42e34f0560e0b7c4b187d25611efcbb06656e6e4
adorsk <adorsk>
2012-02-13 10:09:10 -0500

    added nominal effort per gear model

---
+++ sasi/fishing/nominal_effort_per_gear_model.py
@@ -0,0 +1,31 @@
+from sasi.fishing.effort_model import EffortModel
+from sasi.fishing.effort import Effort
+
+class NominalEffortPerGearModel(EffortModel):
+
+	def __init__(self, grid_model=None, gears=None):
+		self.grid_model = grid_model
+		self.gears = gears
+
+	def get_effort(self, cell=None, time=None):
+
+		# Initialize a list of efforts.
+		efforts = []
+
+		# Get cell for the given location and time.
+		cell = self.grid_model.get_cells(filters={'type': ['km100'], 'id': [cell.id]}).pop()
+
+		# Set the nominal effort to be the cell's area.
+		nominal_effort = cell.area
+
+		# Get the list of gears.
+		gears = self.gears
+		
+		# For each gear...
+		for gear in gears:
+
+			# Create an effort for the gear.
+			efforts.append(Effort(gear=gear, swept_area=effort_per_feature, location=cell, time=time))
+
+		return efforts
+
