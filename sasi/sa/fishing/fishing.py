460527f040089ccc819e435025232312e0913db9
adorsk <adorsk>
2012-02-23 15:24:09 -0500

    fixed gear code mappings, added effort tables to fishing schema creation function, finished script for populating efforts from csv

--- sasi/sa/fishing/fishing.py
+++ sasi/sa/fishing/fishing.py
@@ -1,10 +1,15 @@
 import sasi.sa.session as sa_session
 import sasi.sa.metadata as sa_metadata
 import sasi.sa.fishing.gear as sa_gear
+import sasi.sa.fishing.effort as sa_effort
+import sasi.sa.fishing.effort_set as sa_effort_set
 
 def create_schema():
 	
 	session = sa_session.get_session()
 	sa_gear.table.create(session.bind)
+	sa_effort.table.create(session.bind)
+	sa_effort_set.effort_set_table.create(session.bind)
+	sa_effort_set.effort_set_effort_table.create(session.bind)
 
 
--- sasi/util/fishing/populate_efforts_from_csv.py
+++ sasi/util/fishing/populate_efforts_from_csv.py
@@ -1,9 +1,11 @@
 import csv
 import sasi.conf.conf as conf
+import sasi.conf.gear_code_mappings as gear_code_mappings
 from sasi.dao.fishing.sa_effort_dao import SA_Effort_DAO
 from sasi.dao.habitat.sa_cell_dao import SA_Cell_DAO
 import sasi.sa.session as sa_session
 from sasi.fishing.effort import Effort
+from sasi.fishing.gear import Gear
 from sasi.fishing.effort_set import Effort_Set
 import sys
 
@@ -19,29 +21,50 @@ def main():
 	# Setup DAOs.
 	db_session = sa_session.get_session()
 	effort_dao = SA_Effort_DAO(session = db_session)
-	cell_dao = SA_Cell_DAO(session = db_session, default_filters=[{'attr': 'type', 'op': '==', 'value': 'km100'}])
+	cell_dao = SA_Cell_DAO(session = db_session)
+
+	# Get 100km cells and make id lookup.
+	cells = cell_dao.get_cells(filters=[{'attr': 'type', 'op': '==', 'value': 'km100'}])
+	cells_by_100km_id = {}
+	for c in cells: cells_by_100km_id[c.type_id] = c
+
+	# Make gears lookup.
+	gears_by_dry_code = {}
+	for wet_code, dry_code in gear_code_mappings.wet_to_dry.items():
+		 gear = db_session.query(Gear).filter(Gear.id == dry_code).one()
+		 gears_by_dry_code[dry_code] = gear
 
 	# Create efforts from csv rows.
 	efforts = []
 	csv_reader = csv.DictReader(open(csv_file, "rb"))
+	r_counter = 0
 	for r in csv_reader:
 
+		r_counter += 1
+		if (r_counter % 1000) == 0:
+			print >> sys.stderr, r_counter
+
 		# Get cell.
-		cell = cell_dao.get_cells(filters=[{'attr': 'type_id', 'op': '==', 'value': r['id_100']}])
+		cell = cells_by_100km_id[r['id_100']]
 
 		# Get gear.
-		gear
+		wet_code = "GC%s" % r['gear_code']
+		dry_code = gear_code_mappings.wet_to_dry[wet_code]
+		gear = gears_by_dry_code[dry_code]
 
-		# Get gear.
+		# Create effort.
 		efforts.append(Effort(
-			cell = c,
+			cell = cell,
 			time = r['year'],
-			gear = g,
+			gear = gear,
 			swept_area = float(r['A']),
 			hours_fished = float(r['hours_fished'])
 			))
-		{'A': '0.00732691518963', 'gear_code': '50', 'hours_fished': '53.7511902667856', '_FREQ_': '2', 'id_100': '741', '_TYPE_': '0', 'value': '1114.22815351244', 'year': '1998'}
-
 	
+	# Create effort set from efforts.
+	effort_set = Effort_Set(id=effort_set_id, efforts=efforts)
+
+	# Save effort set.
+	effort_dao.save_effort_sets(effort_sets=[effort_set])
 
 if __name__ == '__main__': main()
