44e937cb06e1361c7b346e8ed775fcc29ed5594b
adorsk <adorsk>
2012-02-23 12:12:06 -0500

    fixed filter format

--- sasi/habitat/tests/test_static_grid_model.py
+++ sasi/habitat/tests/test_static_grid_model.py
@@ -6,7 +6,7 @@ from sasi.dao.habitat.test_cell_dao import Test_Cell_DAO
 class StaticGridModelTest(unittest.TestCase):
 
 	def test(self):
-		grid_model = StaticGridModel(cell_dao=Test_Cell_DAO(), default_filters={'type': ['km100']}) 
+		grid_model = StaticGridModel(cell_dao=Test_Cell_DAO(), default_filters=[{'attr':'type', 'op': '==', 'value': 'km100'}]) 
 
 		cells = grid_model.get_cells()
 
