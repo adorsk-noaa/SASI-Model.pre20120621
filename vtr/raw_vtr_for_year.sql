61ab7f88121b8f77adcfaa921791ee66b941f2bd
adorsk <adorsk>
2012-02-27 17:17:12 -0500

    added to dumping script

--- vtr/raw_vtr_for_year.sql
+++ vtr/raw_vtr_for_year.sql
@@ -4,6 +4,7 @@ t.tripcatg,
 t.datesail,
 t.timesail,
 t.datelnd1 datelnd1, 
+t.permit permit, 
 t.hullnum,
 t.port, 
 t.crew,
@@ -31,15 +32,24 @@ s.qtykept keptlb,
 s.qtydisc disclb,
 s.datesold, 
 s.dealnum,
-a.nespp4
+a.nespp4,
+v.vp_num, 
+v.hport,
+v.hpst,
+v.pport,
+v.ppst,
+v.ap_year,
+v.ves_name,
+v.gtons,
+v.vhp,
+v.len
 
 FROM
-VESLOG{{YEAR}}T t,
-VESLOG{{YEAR}}G g, 
-VESLOG{{YEAR}}S s LEFT JOIN VLSPPTBL a ON s.sppcode = a.sppcode
-
+VESLOG{{YEAR}}T t LEFT JOIN VESLOG{{YEAR}}G g ON t.tripid = g.tripid
+LEFT JOIN VESLOG{{YEAR}}S s ON s.tripid = t.tripid AND s.gearid = g.gearid
+LEFT JOIN VLSPPTBL a on s.sppcode = a.sppcode
+LEFT JOIN VPS_VESSEL v ON t.permit = v.vp_num
 WHERE
-s.tripid=t.tripid
-and s.tripid=g.tripid
-and s.gearid=g.gearid 
-and t.tripcatg in (1, 4)
+t.tripcatg in (1, 4)
+AND v.ap_year = {{YEAR}}
+;
