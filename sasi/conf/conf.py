0f2aaf26544de801541cede380244173e0c2ccfa
adorsk <adorsk>
2012-02-29 11:48:00 -0500

    moved dburi into secrets

--- sasi/conf/conf.py
+++ sasi/conf/conf.py
@@ -1,7 +1,6 @@
 import sasi.conf.secrets as secrets
 
 conf = {
-		'db_uri' : 'postgresql://sasi:sasi@localhost/dev_sasi',
 		'grid_file': '/home/adorsk/projects/sasi/data/Final_SASI_GIS/Standard_Grids.shp',
 		'va_file': '/home/adorsk/projects/sasi/data/vulnerability_assessment/matrices_master.csv',
 		'sasi_habitat_file': '/home/adorsk/projects/sasi/data/Final_SASI_GIS/Grid_SASI.shp',
