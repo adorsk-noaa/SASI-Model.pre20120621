e4d1046b36bb042c0b4c63d0abc1d55117e138e4
adorsk <adorsk>
2012-02-29 12:00:48 -0500

    adding template files, skeleton for viz'

--- sasi/viz/results/map/mapserver.py
+++ sasi/viz/results/map/mapserver.py
@@ -2,5 +2,25 @@
 Functions for creating result maps via MapServer.
 """
 
+import sasi.conf.conf as conf
+
 def get_results_map():
-	pass
+
+	# Get DB connection string.
+
+	# Get raw SQL query for results.
+
+	# Generate color map.
+
+	# Process mapfile template.
+
+	# Write processed template to a tmp mapfile.
+
+	# Create map image from template.
+
+	# Remove tmp mapfile.
+
+	# Crop map image.
+
+	# Return image.
+
