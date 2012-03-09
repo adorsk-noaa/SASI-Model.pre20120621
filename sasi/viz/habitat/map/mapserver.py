57805aed070931054c2e5d7fb24e5542d41988be
adorsk <adorsk>
2012-03-09 10:31:50 -0500

    added wms request to testing

--- sasi/viz/habitat/map/mapserver.py
+++ sasi/viz/habitat/map/mapserver.py
@@ -5,33 +5,38 @@ import mapscript
 import sys, os
 
 # Get a habitat map from a WMS parameters and custom parameters.
-def get_map_image_from_wms(parameters=None, habitat_dao=None):
+def get_map_image_from_wms(wms_parameters=None, habitat_dao=None, filters=None):
 
 	# Initialize mapscript map object from the habitat mapfile template.
 	mapfile = "%s/habitat.map" % os.path.abspath(os.path.dirname(__file__))
 	ms_map = mapscript.mapObj(mapfile)
 
-	"""
 	# Build WMS request from parameters.
 	wms_request = mapscript.OWSRequest()
-	for k, v in parameters.get('wms', {}).items():
+	for k, v in wms_parameters.items():
 		wms_request.setParameter(k,v)
-	
+
 	# Load the parameters into the map.
 	ms_map.loadOWSParameters(wms_request)
-	"""
+
+	# Get the habitat layer.
+	layer = ms_map.getLayerByName('habitat')
+
+	# Set connection type to be POSTGIS.
+	layer.setConnectionType(mapscript.MS_POSTGIS, '')
 
 	# Set the habitat layer's connection parameters.
-	connection_str = ""
+	connection_str = habitat_dao.get_mapserver_connection_string()
+	layer.connection = connection_str
 	
 	# Build data string based on custom parameters.
-	data_str = ""
+	data_str = habitat_dao.get_mapserver_data_string(filters=filters)
+	layer.data = data_str
 	
 	# Draw the map.
 	ms_image = ms_map.draw()
 
 	# Return the raw image.
 	return ms_image.getBytes()
-	"""
 
 
--- sasi/viz/habitat/map/tests/test_mapserver.py
+++ sasi/viz/habitat/map/tests/test_mapserver.py
@@ -1,10 +1,30 @@
 import unittest
 import sasi.viz.habitat.map.mapserver as habitat_ms
+from sasi.dao.habitat.sa_habitat_dao import SA_Habitat_DAO
+from sasi.sa.tests.basetest import BaseTest
 
-class Habitat_Mapserver_Test(unittest.TestCase):
+class Habitat_Mapserver_Test(BaseTest):
 
 	def test(self):
-		habitat_ms.get_map_image_from_wms(parameters=None, habitat_dao=None)
+		s = self.session
+		habitat_dao = SA_Habitat_DAO(session=s)
+
+		# Generate WMS request parameters.
+		wms_parameters = {
+				'SERVICE': 'WMS' ,
+				'VERSION': '1.1.0', 
+				'REQUEST': 'GetMap', 
+				'LAYERS': 'habitat',
+				'SRS':'EPSG:4326',
+				'BBOX':'-180.0,-90.0,180.0,90.0',
+				'FORMAT':'image/gif',
+				'WIDTH':'640',
+				'HEIGHT':'640'
+				}
+		
+		img = habitat_ms.get_map_image_from_wms(wms_parameters=wms_parameters, habitat_dao=habitat_dao)
+		print img
+
 		self.failUnless(True)
 
 
