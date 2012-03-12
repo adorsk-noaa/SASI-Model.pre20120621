035ba8bcbc0d00164f4472700d6db0621a980fe2
adorsk <adorsk>
2012-03-12 09:39:56 -0400

    added features to habitat_type, instead of doing separate lookup.  This makes filtering, stats calculation by feature more convenient

--- sasi/habitat/habitat_type.py
+++ sasi/habitat/habitat_type.py
@@ -1,7 +1,8 @@
 class Habitat_Type(object):
 
-	def __init__(self, id=None, substrate=None, energy=None):
+	def __init__(self, id=None, substrate=None, energy=None, features=None):
 		self.substrate = substrate
 		self.energy = energy
+		self.features = features
 		self.id = "%s,%s" % (self.substrate.id,self.energy)
 
--- sasi/sa/habitat/habitat_type.py
+++ sasi/sa/habitat/habitat_type.py
@@ -5,24 +5,32 @@ from geoalchemy.postgis import PGComparator
 
 from sasi.habitat.habitat_type import Habitat_Type
 from sasi.habitat.substrate import Substrate
+from sasi.habitat.feature import Feature
 import sasi.sa.habitat.substrate as sa_substrate
+import sasi.sa.habitat.feature as sa_feature
 
 import sasi.sa.metadata as sa_metadata
 metadata = sa_metadata.metadata
 
-table = Table('habitat_type', metadata,
+habitat_type_table = Table('habitat_type', metadata,
 		Column('id', String, primary_key=True),
 		Column('substrate_id', String),
 		Column('energy', String),
 		ForeignKeyConstraint(['substrate_id'], [sa_substrate.table.c.id], deferrable=True),
 		)
 
-GeometryDDL(table)
+habitat_type_feature_table = Table('habitat_type_feature', metadata,
+		Column('habitat_type_id', String, primary_key=True),
+		Column('feature_id', String, primary_key=True),
+		ForeignKeyConstraint(['habitat_type_id'], [habitat_type_table.c.id], deferrable=True),
+		ForeignKeyConstraint(['feature_id'], [sa_feature.table.c.id], deferrable=True),
+		)
 
 mapper(
 		Habitat_Type,
-		table,
+		habitat_type_table,
 		properties = {
 		'substrate': relationship(Substrate, lazy='joined'),
+		'features': relationship(Feature, secondary=habitat_type_feature_table, lazy='subquery')
 		})
 
