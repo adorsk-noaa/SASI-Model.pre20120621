acff891a53eff68471a581d265a366ea7d89500a
adorsk <adorsk>
2012-03-22 11:01:30 -0400

    w/ tile stub still in

--- sasi/viz/habitat/map/mapserver.py
+++ sasi/viz/habitat/map/mapserver.py
@@ -2,11 +2,11 @@
 Mapserver implementations of mapping methods for habitat maps.
 """
 import mapscript
-import sys, os
+import sys, os, re, struct
 
 # Get a habitat map from a WMS parameters and custom parameters.
 # @TODO: empirically, nx=12 and maxfeatures=1000 gets reasonable results.
-def get_map_image_from_wms(wms_parameters=None, habitat_dao=None, filters=[]):
+def foo_get_map_image_from_wms(wms_parameters=None, habitat_dao=None, filters=[]):
 
 	# Initialize mapscript map object from the habitat mapfile template.
 	mapfile = "%s/habitat.map" % os.path.abspath(os.path.dirname(__file__))
@@ -16,7 +16,8 @@ def get_map_image_from_wms(wms_parameters=None, habitat_dao=None, filters=[]):
 	# Generate tiled data layers.
 
 	# Get bounds for each tile.
-	nx = 12
+	maxfeatures = 200
+	nx = 50
 	ny = nx
 	minx = -79.0
 	miny = 31.0
@@ -35,40 +36,97 @@ def get_map_image_from_wms(wms_parameters=None, habitat_dao=None, filters=[]):
 	for xp in xpairs:
 		for yp in ypairs:
 			tiles.append([xp[0], yp[0], xp[1], yp[1]])
+
+	tiles = [[-69.25, 40.78, -69.12, 40.88]]
 	
+
+	# Get data string template.
+	tile_stub = 'POLYGON((TILEPOLYGON))'
+	tile_filter =  [{'field': 'geom', 'op': 'intersects', 'value': tile_stub}]
+	data_str_template = habitat_dao.get_mapserver_data_string(filters= filters + tile_filter)
+
 	# Define a layer for each tile.
 	i = 0
+	outline_layers = []
+
+	#for t in tiles[63:65]:
 	for t in tiles:
 		layer = mapscript.layerObj()
 		layer.name = "layer_%s" % i 
 		layer.group = 'data'
 
+		layer.debug = 2
+
 		layer.type = mapscript.MS_LAYER_POLYGON
-
-		layer.setConnectionType(mapscript.MS_POSTGIS, '')
-		connection_str = habitat_dao.get_mapserver_connection_string()
-		layer.connection = connection_str
-		data_str = habitat_dao.get_mapserver_data_string(filters=filters)
-		layer.data = data_str
-		layer.setProcessingKey('CLOSE_CONNECTION', 'DEFER')
-
-		layer.maxfeatures = 1000
-
 		layer.setExtent(*t)
 
 		layer.setProjection('init=epsg:4326')
 
+		layer.status = mapscript.MS_DEFAULT
+
+		# Create inline feature layer for show tile bounds.
+		outline_layer = layer.clone()
+		outline_layer.name = "outline_layer_%s" % i 
+		outline_clz = mapscript.classObj()
+		outline_clz.name = 'clz'
+		outline_style = mapscript.styleObj()
+		outline_style.outlinecolor= mapscript.colorObj(0,0,0)
+		outline_style.width = 3
+		outline_clz.insertStyle(outline_style)
+		outline_layer.insertClass(outline_clz)
+		outline_feature = mapscript.shapeObj(mapscript.MS_SHAPE_POLYGON)
+		tile_wkt = "POLYGON((%s %s, %s %s, %s %s, %s %s, %s %s))" % (t[0], t[1], t[0], t[3], t[2], t[3], t[2], t[1], t[0], t[1])
+		outline_feature = outline_feature.fromWKT(tile_wkt)
+		outline_layer.addFeature(outline_feature)
+		outline_layers.append(outline_layer)
+
+
+		layer.setConnectionType(mapscript.MS_POSTGIS, '')
+		connection_str = habitat_dao.get_mapserver_connection_string()
+		layer.connection = connection_str
+
+		# Replace geo filter in data string with tile bounds.
+		data_str = re.sub(re.escape(tile_stub), tile_wkt, data_str_template)
+		layer.data = data_str
+		layer.setProcessingKey('CLOSE_CONNECTION', 'DEFER')
+
+		line_layer = layer.clone()
+		line_clz = mapscript.classObj()
+		line_clz.name = 'clz'
+		line_style = mapscript.styleObj()
+		line_style.outlinecolor= mapscript.colorObj(0,0,0)
+		line_style.width = .5 
+		line_clz.insertStyle(line_style)
+		line_layer.insertClass(line_clz)
+		ms_map.insertLayer(line_layer)
+
+	
+		layer.maxfeatures = maxfeatures
+
+
+
 		clz = mapscript.classObj()
 		clz.name = 'clz'
 		style = mapscript.styleObj()
-		style.color= mapscript.colorObj((i * 10) % 255,1 + ((i*10) % 255)/2,1)
+		#style.color= mapscript.colorObj((i) % 255, 1,1)
+		style.color= mapscript.colorObj(128, 1,1)
+		style.outlinecolor= mapscript.colorObj(0,0,0)
+		style.width = .5 
 		clz.insertStyle(style)
 		layer.insertClass(clz)
 
-		layer.status = mapscript.MS_DEFAULT
 
 		ms_map.insertLayer(layer)
+
 		i += 1
+	
+	"""
+	for ol in outline_layers:
+		ms_map.insertLayer(ol)
+	"""
+	
+	pid = os.getpid()
+	ms_map.setConfigOption('MS_ERRORFILE', '/tmp/map_log.%s.txt' % pid)
 	
 	# Build WMS request from parameters.
 	wms_request = mapscript.OWSRequest()
@@ -84,13 +142,48 @@ def get_map_image_from_wms(wms_parameters=None, habitat_dao=None, filters=[]):
 	# Return the raw image.
 	return ms_image.getBytes()
 
-"""
 def get_map_image_from_wms(wms_parameters=None, habitat_dao=None, filters=None):
 
 	# Initialize mapscript map object from the habitat mapfile template.
 	mapfile = "%s/habitat.map" % os.path.abspath(os.path.dirname(__file__))
 	ms_map = mapscript.mapObj(mapfile)
 
+	layer = mapscript.layerObj()
+	layer.name = "data" 
+	layer.group = 'data'
+
+	layer.setProjection('init=epsg:4326')
+	layer.status = mapscript.MS_DEFAULT
+	layer.setConnectionType(mapscript.MS_POSTGIS, '')
+	connection_str = habitat_dao.get_mapserver_connection_string()
+	layer.connection = connection_str
+	layer.data = habitat_dao.get_mapserver_data_string(filters=filters)
+
+	layer.type = mapscript.MS_LAYER_POLYGON
+
+	# Create classes for types of substrates.
+
+	substrate_styles= [
+			{'name': 'S1', 'color': '8DD3C7'},
+			{'name': 'S2', 'color': 'FFFFB3'},
+			{'name': 'S3', 'color': 'BEBADA'},
+			{'name': 'S4', 'color': 'FB8072'},
+			{'name': 'S5', 'color': '80B1D3'},
+			]
+
+	for ss in substrate_styles:
+		clz = mapscript.classObj()
+		clz.name = ss['name']
+		expression = "('[substrate_id]' eq '%s')" % ss['name']
+		clz.setExpression(expression)
+		style = mapscript.styleObj()
+		rgb_color = hex_to_rgb(ss['color'])
+		style.color= mapscript.colorObj(*rgb_color)
+		clz.insertStyle(style)
+		layer.insertClass(clz)
+
+	ms_map.insertLayer(layer)
+
 	# Build WMS request from parameters.
 	wms_request = mapscript.OWSRequest()
 	for k, v in wms_parameters:
@@ -98,25 +191,15 @@ def get_map_image_from_wms(wms_parameters=None, habitat_dao=None, filters=None):
 
 	# Load the parameters into the map.
 	ms_map.loadOWSParameters(wms_request)
-
-	# Get the habitat layer.
-	layer = ms_map.getLayerByName('habitat')
-
-	# Set connection type to be POSTGIS.
-	layer.setConnectionType(mapscript.MS_POSTGIS, '')
-
-	# Set the habitat layer's connection parameters.
-	connection_str = habitat_dao.get_mapserver_connection_string()
-	layer.connection = connection_str
-	
-	# Build data string based on custom parameters.
-	data_str = habitat_dao.get_mapserver_data_string(filters=filters)
-	layer.data = data_str
 	
 	# Draw the map.
 	ms_image = ms_map.draw()
 
 	# Return the raw image.
 	return ms_image.getBytes()
-"""
 
+def hex_to_rgb(hexstr): 
+	return struct.unpack('BBB',hexstr.decode('hex'))
+
+def rgb_to_hex(rgb): 
+	struct.pack('BBB',*rgb).encode('hex')
