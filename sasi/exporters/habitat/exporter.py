d03607e956748cd501d3457b638b601de37743a7
adorsk <adorsk>
2012-03-30 15:54:46 -0400

    fixed targz export

--- sasi/exporters/habitat/exporter.py
+++ sasi/exporters/habitat/exporter.py
@@ -2,6 +2,7 @@ import shapely.wkb
 import json
 import tempfile
 import zipfile
+import tarfile
 import os
 
 class Exporter(object):
@@ -33,12 +34,27 @@ class Exporter(object):
 		return tempfile.mkdtemp(*args, **kwargs)
 	
 
-	def make_package(self, export_dir=None, package_name="habitats"):
-		# create zip file from export files.
+	def make_package(self, package_type='tgz', **kwargs):
+		if package_type == 'zip': 
+			return self.make_zip_package(**kwargs)
+
+		elif package_type == 'tgz': 
+			return self.make_tar_package(**kwargs)
+
+
+	def make_zip_package(self, export_dir=None, package_name="habitats"):
 		zfile_path = tempfile.mktemp(suffix=".zip")
 		zfile_h = zipfile.ZipFile(zfile_path, "w")
 		for f in os.listdir(export_dir):
 			zfile_h.write("{}/{}".format(export_dir, f), "{}/{}".format(package_name, os.path.basename(f)))
 		zfile_h.close()
 		return zfile_path
+
+	def make_tar_package(self, export_dir=None, package_name="habitats"):
+		tarfile_path = tempfile.mktemp(suffix=".tgz")
+		tarfile_h = tarfile.open(tarfile_path, "w:gz")
+		for f in os.listdir(export_dir):
+			tarfile_h.add("{}/{}".format(export_dir, f), "{}/{}".format(package_name, os.path.basename(f)))
+		tarfile_h.close()
+		return tarfile_path
 
