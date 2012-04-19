c14e87700c4eeb325742fea7e8b303b22fbe68ad
adorsk <adorsk>
2012-04-19 10:27:17 -0400

    added method to close sessions.  scoped sessions need to be closed manually, otherwise they accumulate w/ mod_wsgi

--- sasi/sa/session.py
+++ sasi/sa/session.py
@@ -2,6 +2,10 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 import sasi.sa.engine as sa_engine
 
+Session = scoped_session(sessionmaker(bind=sa_engine.get_engine()))
+
 def get_session():
-	Session = scoped_session(sessionmaker(bind=sa_engine.get_engine()))
 	return Session()
+
+def close_session():
+	Session.remove()
